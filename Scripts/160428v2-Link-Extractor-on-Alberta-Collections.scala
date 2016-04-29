ef43a256e4dae4472f3ec1cf42bf7fb7f5868ad0
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-04-29 09:19:46 -0400

    checking in scripts for 29 Apr

---
+++ Scripts/160428v2-Link-Extractor-on-Alberta-Collections.scala
@@ -0,0 +1,38 @@
+import org.warcbase.spark.matchbox.{ExtractDomain, ExtractLinks, RecordLoader}
+import org.warcbase.spark.rdd.RecordRDD._
+
+RecordLoader.loadArchives("/data/lfrancophonie_de_louest_canadien/*.gz", sc)
+  .keepValidPages()
+  .map(r => (r.getCrawlDate, ExtractLinks(r.getUrl, r.getContentString)))
+  .flatMap(r => r._2.map(f => (r._1, ExtractDomain(f._1).replaceAll("^\\s*www\\.", ""), ExtractDomain(f._2).replaceAll("^\\s*www\\.", ""))))
+  .filter(r => r._2 != "" && r._3 != "")
+  .countItems()
+  .filter(r => r._2 > 5)
+  .saveAsTextFile("/data/derivatives/links/lfrancophonie_de_louest_canadien")
+
+RecordLoader.loadArchives("/data/ottawa_shooting_october_2014/*.gz", sc)
+  .keepValidPages()
+  .map(r => (r.getCrawlDate, ExtractLinks(r.getUrl, r.getContentString)))
+  .flatMap(r => r._2.map(f => (r._1, ExtractDomain(f._1).replaceAll("^\\s*www\\.", ""), ExtractDomain(f._2).replaceAll("^\\s*www\\.", ""))))
+  .filter(r => r._2 != "" && r._3 != "")
+  .countItems()
+  .filter(r => r._2 > 5)
+  .saveAsTextFile("/data/derivatives/links/ottawa_shooting_october_2014")
+
+RecordLoader.loadArchives("/data/prarie_provinces/*.gz", sc)
+  .keepValidPages()
+  .map(r => (r.getCrawlDate, ExtractLinks(r.getUrl, r.getContentString)))
+  .flatMap(r => r._2.map(f => (r._1, ExtractDomain(f._1).replaceAll("^\\s*www\\.", ""), ExtractDomain(f._2).replaceAll("^\\s*www\\.", ""))))
+  .filter(r => r._2 != "" && r._3 != "")
+  .countItems()
+  .filter(r => r._2 > 5)
+  .saveAsTextFile("/data/derivatives/links/prarie_provinces")
+
+RecordLoader.loadArchives("/data/web_archive_general/*.gz", sc)
+  .keepValidPages()
+  .map(r => (r.getCrawlDate, ExtractLinks(r.getUrl, r.getContentString)))
+  .flatMap(r => r._2.map(f => (r._1, ExtractDomain(f._1).replaceAll("^\\s*www\\.", ""), ExtractDomain(f._2).replaceAll("^\\s*www\\.", ""))))
+  .filter(r => r._2 != "" && r._3 != "")
+  .countItems()
+  .filter(r => r._2 > 5)
+  .saveAsTextFile("/data/derivatives/links/web_archive_general")
